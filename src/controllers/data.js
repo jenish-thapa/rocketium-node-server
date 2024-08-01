d578fee252b50db520161af1a6646a619bddc34e
Beserker-356 <jenish.23bcs10016@ms.sst.scaler.com>
2024-08-01 16:06:25 +0530

    Implemented Sorting, Ordering, Field Query, Limit and Offset

--- src/controllers/data.js
+++ src/controllers/data.js
@@ -1,7 +1,53 @@
+const url = require("url");
 const data = require("../data/dummyData.json");
 
+const fields = ["name", "language", "id", "bio", "version"];
+
 const getData = (req, res) => {
-  res.json(data);
+  const query = url.parse(req.url, true).query;
+
+  console.log(query);
+
+  let filteredData = [...data];
+
+  for (const key in query) {
+    if (key in fields) {
+      const value = query[key];
+      filteredData = filteredData.filter((item) => {
+        if (typeof item[key] === "number") {
+          return item[key] === parseFloat(value);
+        } else if (typeof item[key] === "string") {
+          return item[key].toLowerCase() === value.toLowerCase();
+        }
+        return false;
+      });
+    }
+  }
+
+  const sortBy = query.sortBy;
+  const order = query.order;
+
+  if (typeof sortBy === "string" && filteredData.length > 0) {
+    filteredData.sort((a, b) => {
+      if (typeof a[sortBy] === "string") {
+        return a[sortBy].localeCompare(b[sortBy]);
+      } else if (typeof a[sortBy] === "number") {
+        return a[sortBy] - b[sortBy];
+      }
+      return 0;
+    });
+
+    if (order === "desc") {
+      filteredData.reverse();
+    }
+  }
+
+  const limit = parseInt(req.query.limit, 10) || filteredData.length;
+  const offset = parseInt(req.query.offset, 10) || 0;
+
+  const paginatedData = filteredData.slice(offset, offset + limit);
+
+  res.json(paginatedData);
 };
 
 module.exports = { getData };
